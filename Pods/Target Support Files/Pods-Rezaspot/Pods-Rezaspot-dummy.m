dc51b96ed7838b34eff844b27c0af759563bd9a5
Shreyesh <imshreyesh17@gmail.com>
2019-08-20 00:46:29 +0530

    app initialized

---
+++ Pods/Target Support Files/Pods-Rezaspot/Pods-Rezaspot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rezaspot : NSObject
+@end
+@implementation PodsDummy_Pods_Rezaspot
+@end
---
+++ Rezaspot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rezaspot
+//
+//  Created by Shreyesh on 8/20/19.
+//  Copyright © 2019 Quark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
